e4ca2c2234fe2f46b353686e742ad7bb9cfb2af6
MeghPatel-007 <meghpatel0009@gmail.com>
2026-03-10 20:08:11 +0530

    setup done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import {browserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from "./pages/login.jsx";
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
